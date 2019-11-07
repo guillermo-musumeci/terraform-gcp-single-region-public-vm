05203249f0c2b17e6955db059d9093c40a7e39e1
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2019-11-07 18:42:00 +0100

    Initial commit

--- network-firewall.tf
+++ network-firewall.tf
@@ -2,7 +2,7 @@
 
 # Allow http
 resource "google_compute_firewall" "allow-http" {
-  name    = "${var.app_name}-fw-allow-http"
+  name    = "${var.app_name}-${var.app_environment}-fw-allow-http"
   network = "${google_compute_network.vpc.name}"
   allow {
     protocol = "tcp"
@@ -13,7 +13,7 @@ resource "google_compute_firewall" "allow-http" {
 
 # allow https
 resource "google_compute_firewall" "allow-https" {
-  name    = "${var.app_name}-fw-allow-https"
+  name    = "${var.app_name}-${var.app_environment}-fw-allow-https"
   network = "${google_compute_network.vpc.name}"
   allow {
     protocol = "tcp"
@@ -24,7 +24,7 @@ resource "google_compute_firewall" "allow-https" {
 
 # allow ssh
 resource "google_compute_firewall" "allow-ssh" {
-  name    = "${var.app_name}-fw-allow-ssh"
+  name    = "${var.app_name}-${var.app_environment}-fw-allow-ssh"
   network = "${google_compute_network.vpc.name}"
   allow {
     protocol = "tcp"
@@ -35,7 +35,7 @@ resource "google_compute_firewall" "allow-ssh" {
 
 # allow rdp
 resource "google_compute_firewall" "allow-rdp" {
-  name    = "${var.app_name}-fw-allow-rdp"
+  name    = "${var.app_name}-${var.app_environment}-fw-allow-rdp"
   network = "${google_compute_network.vpc.name}"
   allow {
     protocol = "tcp"
